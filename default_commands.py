77d946b2cec308a61f5e5f6e6032c6bc56a3585e
JL710 <jan.lyncker@freenet.de>
2022-06-25 20:34:15 +0200

    Update default_commands.py

--- default_commands.py
+++ default_commands.py
@@ -1,9 +1,10 @@
 import discord
 
 
-def load(tree, guild_id):
-    @tree.command(guild = discord.Object(id=guild_id), name="ping", description="Just a Ping")
+def load(tree):
+    @tree.command(name="ping", description="Just a Ping")
     async def ping(interaction: discord.Interaction):
         embed = discord.Embed(title="Ping", description="I am Online!")
         await interaction.response.send_message(embed=embed)
         raise ValueError("Pong")
+        
