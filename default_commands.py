b8264a8f3c23c4db310c3ecd5aba0b35a5946dca
JL710 <jan.lyncker@freenet.de>
2022-06-25 19:05:49 +0200

    better ping

--- default_commands.py
+++ default_commands.py
@@ -6,4 +6,4 @@ def load(tree, guild_id):
     async def ping(interaction: discord.Interaction):
         embed = discord.Embed(title="Ping", description="I am Online!")
         await interaction.response.send_message(embed=embed)
-        
+        raise ValueError("Pong")
