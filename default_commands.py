25b589f460082798c48bc0f242e762bb260bd5e1
JL710 <jan.lyncker@freenet.de>
2022-06-26 14:49:04 +0200

    Update default_commands.py

--- default_commands.py
+++ default_commands.py
@@ -3,7 +3,7 @@ import discord
 
 def load(tree):
     @tree.command(name="ping", description="Just a Ping!")
-    @discord.app_commands.checks.cooldown(60, 60)
+    @discord.app_commands.checks.cooldown(1, 60, key=lambda i: i.user.id)
     async def ping(interaction: discord.Interaction):
         embed = discord.Embed(title="Ping", description="I am Online!")
         await interaction.response.send_message(embed=embed)
